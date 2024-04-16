65c8e517319c04bcaca5d25a9cd2a1c7a9c96302
joyce <joyce069.js@gmail.com>
2024-04-16 15:29:16 -0300

    criando entidade Cliente e a classe DaoGenerico

--- src/main/java/br/com/AutoPecasLoja/model/Cliente.java
+++ src/main/java/br/com/AutoPecasLoja/model/Cliente.java
@@ -1,4 +1,44 @@
 package br.com.AutoPecasLoja.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true, nullable = false)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String nome;
+
+    public Cliente() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
+
