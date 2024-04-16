a2988f47eadfb8766440cb3646f200eb3fa40afe
joyce <joyce069.js@gmail.com>
2024-04-16 16:34:57 -0300

    criando ClienteDao

--- src/main/java/br/com/AutoPecasLoja/dao/ClienteDao.java
+++ src/main/java/br/com/AutoPecasLoja/dao/ClienteDao.java
@@ -1,4 +1,81 @@
 package br.com.AutoPecasLoja.dao;
 
+import br.com.AutoPecasLoja.model.Cliente;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
 public class ClienteDao {
+    DaoGenerico<Cliente> daoGenerico = new DaoGenerico<Cliente>();
+
+    public void inserirCliente(Cliente cliente) {
+        daoGenerico.transacaoCompleta(cliente);
+//		daoGenerico.fechar();
+    }
+
+    public List<Cliente> listarTodosClientes() {
+        List<Cliente> clientes = null;
+        try {
+            TypedQuery<Cliente> query = daoGenerico.getConexao()
+                    .createQuery("SELECT c FROM Cliente c ORDER BY c.id", Cliente.class);
+            clientes = query.getResultList();
+        } catch (Exception e) {
+            System.out.println("Erro ao listar clientes: " + e.getMessage());
+        }
+        return clientes;
+    }
+
+    public void atualizarCliente(int id, Cliente novoCliente) {
+        try {
+            // Verifica se o cliente com o ID fornecido existe no banco de dados
+            Cliente clienteExistente = buscarClientePorId(id);
+
+            // Se o cliente existir, atualiza os seus dados com os do novo cliente
+            if (clienteExistente != null) {
+                clienteExistente.setNome(novoCliente.getNome());
+                clienteExistente.setCpf(novoCliente.getCpf());
+
+                daoGenerico.abrirTransacao();
+                daoGenerico.getConexao().merge(clienteExistente);
+                daoGenerico.commitTransacao();
+            } else {
+                System.out.println("Cliente com o ID " + id + " não encontrado.");
+            }
+        } catch (Exception e) {
+            daoGenerico.rollbackTransacao();
+            System.out.println("Erro ao atualizar cliente: " + e.getMessage());
+        }
+    }
+
+    public void deletarCliente(int id) {
+        try {
+            // Verifica se o cliente com o ID fornecido existe no banco de dados
+            Cliente clienteExistente = buscarClientePorId(id);
+
+            // Se o cliente existir, deleta-o do banco de dados
+            if (clienteExistente != null) {
+                daoGenerico.abrirTransacao();
+                daoGenerico.getConexao().remove(clienteExistente);
+                daoGenerico.commitTransacao();
+                System.out.println("Cliente com o ID " + id + " foi deletado com sucesso.");
+            } else {
+                System.out.println("Cliente com o ID " + id + " não encontrado. Não foi possível deletar.");
+            }
+        } catch (Exception e) {
+            daoGenerico.rollbackTransacao();
+            System.out.println("Erro ao deletar cliente: " + e.getMessage());
+        }
+    }
+
+
+    public Cliente buscarClientePorId(int id) {
+        try {
+            // Verifica se o cliente com o ID fornecido existe no banco de dados
+            return daoGenerico.getConexao().find(Cliente.class, id);
+        } catch (Exception e) {
+            System.out.println("Erro ao buscar cliente por ID: " + e.getMessage());
+            return null;
+        }
+    }
+
 }
--- src/main/java/br/com/AutoPecasLoja/testes/ClienteTeste.java
+++ src/main/java/br/com/AutoPecasLoja/testes/ClienteTeste.java
@@ -1,4 +1,73 @@
 package br.com.AutoPecasLoja.testes;
 
+import br.com.AutoPecasLoja.dao.ClienteDao;
+import br.com.AutoPecasLoja.model.Cliente;
+
+import java.util.List;
+
 public class ClienteTeste {
+    public static void main(String[] args) {
+        Cliente cliente1 = new Cliente();
+        Cliente cliente2 = new Cliente();
+        Cliente cliente3 = new Cliente();
+        Cliente cliente4 = new Cliente();
+        Cliente cliente5 = new Cliente();
+        Cliente cliente6 = new Cliente();
+        Cliente cliente7 = new Cliente();
+
+        ClienteDao clienteDao = new ClienteDao();
+
+//		Criando novos clientes
+        cliente1.setCpf("965.778.894-04");
+        cliente1.setNome("Rosa");
+        clienteDao.inserirCliente(cliente1);
+
+        cliente2.setCpf("123.456.789-01");
+        cliente2.setNome("João");
+        clienteDao.inserirCliente(cliente2);
+
+        cliente3.setCpf("987.654.321-09");
+        cliente3.setNome("Maria");
+        clienteDao.inserirCliente(cliente3);
+
+        cliente4.setCpf("456.789.123-45");
+        cliente4.setNome("Carlos");
+        clienteDao.inserirCliente(cliente4);
+
+        cliente5.setCpf("321.654.987-78");
+        cliente5.setNome("Ana");
+        clienteDao.inserirCliente(cliente5);
+
+        cliente6.setCpf("789.123.456-02");
+        cliente6.setNome("Paulo");
+        clienteDao.inserirCliente(cliente6);
+
+        cliente7.setCpf("654.321.987-87");
+        cliente7.setNome("Laura");
+        clienteDao.inserirCliente(cliente7);
+//
+        List<Cliente> clientes = clienteDao.listarTodosClientes();
+
+//		Listar todos os clientes
+        for (Cliente clienteImprimir : clientes) {
+            System.out.println("ID: " + clienteImprimir.getId() + ", Nome: " + clienteImprimir.getNome() + ", CPF: " + clienteImprimir.getCpf());
+        }
+//
+// 		Atualizar
+        cliente2.setNome("Judite");
+        cliente2.setCpf("632.822.546.33");
+        clienteDao.atualizarCliente(2,cliente2);
+
+        //Listar todos os clientes
+        for (Cliente clienteImprimir : clientes) {
+            System.out.println("ID: " + clienteImprimir.getId() + ", Nome: " + clienteImprimir.getNome() + ", CPF: " + clienteImprimir.getCpf());
+        }
+
+        // Deletar cliente com o ID 1
+        clienteDao.deletarCliente(1);
+
+        for (Cliente clienteImprimir : clientes) {
+            System.out.println("ID: " + clienteImprimir.getId() + ", Nome: " + clienteImprimir.getNome() + ", CPF: " + clienteImprimir.getCpf());
+        }
+    }
 }
